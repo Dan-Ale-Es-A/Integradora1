0975a18bd10e854837582cf2c2834e73951fcf51
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2022-03-13 00:15:47 -0500

    Creación de la interfaz HalfRoom

---
+++ src/control/HalfRoomController.java
@@ -0,0 +1,353 @@
+package control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+
+public class HalfRoomController implements Initializable{
+	@FXML
+    private Button b6;
+
+    @FXML
+    private Button b7;
+
+    @FXML
+    private Button b5;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b8;
+
+    @FXML
+    private Button b9;
+
+    @FXML
+    private Button b10;
+
+    @FXML
+    private Button b11;
+
+    @FXML
+    private Button b12;
+
+    @FXML
+    private Button b13;
+
+    @FXML
+    private Button b14;
+
+    @FXML
+    private Button b15;
+
+    @FXML
+    private Button b16;
+
+    @FXML
+    private Button b17;
+
+    @FXML
+    private Button b18;
+
+    @FXML
+    private Button b19;
+
+    @FXML
+    private Button b20;
+
+    @FXML
+    private Button b21;
+
+    @FXML
+    private Button b22;
+
+    @FXML
+    private Button b23;
+
+    @FXML
+    private Button b24;
+
+    @FXML
+    private Button b25;
+
+    @FXML
+    private Button b26;
+
+    @FXML
+    private Button b27;
+
+    @FXML
+    private Button b28;
+
+    @FXML
+    private Button b29;
+
+    @FXML
+    private Button b30;
+
+    @FXML
+    private Button b31;
+
+    @FXML
+    private Button b32;
+
+    @FXML
+    private Button b33;
+
+    @FXML
+    private Button b34;
+
+    @FXML
+    private Button b35;
+
+    @FXML
+    private Button b36;
+
+    @FXML
+    private Button b37;
+
+    @FXML
+    private Button b38;
+
+    @FXML
+    private Button b39;
+
+    @FXML
+    private Button b40;
+
+    @FXML
+    private Button b41;
+
+    @FXML
+    private Button b42;
+
+    @FXML
+    void clickb1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb10(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb11(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb12(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb13(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb14(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb15(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb16(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb17(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb18(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb19(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb20(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb21(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb22(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb23(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb24(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb25(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb26(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb27(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb28(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb29(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb30(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb31(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb32(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb33(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb34(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb35(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb36(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb37(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb38(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb39(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb40(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb41(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb42(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickb9(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
